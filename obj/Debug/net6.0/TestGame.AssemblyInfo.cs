7d6ec4f19d8e19081545160b7492b26c0e9a0c90
Sassan Zomorodi <sassanzomorodi@yahoo.com>
2024-06-24 11:21:27 +0200

    Updated .gitignore

--- obj/Debug/net6.0/TestGame.AssemblyInfo.cs
+++ obj/Debug/net6.0/TestGame.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77d7b9a57b12e0c7309d314f0a27f6c52679d2f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
